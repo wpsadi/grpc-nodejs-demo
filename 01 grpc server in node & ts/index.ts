de3aaaa2b1634bb05b85f0e3ff79b9ed33cb238a
Aditya <wpsadi@outlook.com>
2025-08-02 09:23:12 +0000

    learning basics

---
+++ 01 grpc server in node & ts/index.ts
@@ -0,0 +1,8 @@
+import path from "path";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+
+
+const PORT = process.env.PORT || "8000";
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # grpc-nodejs-demo
+
+This contains demonstratuion for usage of grpc in Node.js applications.
